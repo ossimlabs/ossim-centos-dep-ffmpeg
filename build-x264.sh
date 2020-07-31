3a68723ae4a919c92678bc9c036d91a1253da830
Milton <milton.stafford.iii@gmail.com>
2020-07-31 13:55:42 -0400

    fix script location

--- build-x264.sh
+++ build-x264.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd x264
+cd x264/x264-0.155-20180923-545de2f
 ./configure --enable-shared --prefix=/usr/local --disable-asm
 make -j 8 install install-lib-static install-lib-shared
 if [ $? -ne 0 ]; then echo "x264 install error: $error" ; exit 1 ; fi
